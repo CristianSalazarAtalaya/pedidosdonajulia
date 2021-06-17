cfa8ac4174a3719d417891f33ee395d541c2401b
Cristian Salazar Atalaya <cristiansalazaratalaya@hotmail.com>
2021-06-17 03:40:59 -0500

    Se Agregó el entorno virtual

--- tests/main.py
+++ tests/main.py
@@ -1,3 +1,31 @@
-import unittest
+#from unittest import TestCase, result
+#from .utils import classes
 
-print("holaaa")
+
+
+# class TestResponse(TestCase):
+    
+#     def test_ok(self):
+#         result =
+
+import pytest
+
+@pytest.fixture
+def numbers():
+    a = 10
+    b=20
+    c =25
+    return[a,b,c]
+
+
+def test_method(numbers):
+    x= 15
+    assert numbers[0] == X
+
+def test_method2(numbers):
+    y = 20
+    assert numbers[1] == y
+
+def test_method2(numbers):
+    z = 25
+    assert numbers[2] == z
